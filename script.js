c070978fa0b7c332262ee9a63ee6043bb432da49
minhho3199 <cryaosis132@gmail.com>
2021-03-28 14:15:35 +0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+// var navbar = document.getElementById("nav");
+// window.onscroll = function() {
+//     if(document.body.scrollTop >= 100)
+//     {
+//         navbar.style.backgroundColor = "#181818";
+//     }
+//     else {
+//         navbar.style.backgroundColor = "transparent";
+//     }
+// }
+// $(window).scroll(function(){
+//     if($("body").scrollTop() >= 600) {
+//         $("nav").css("backgroundColor", "#181818");
+//     }
+//     else {
+//         $("nav").css("backgroundColor", "transparent");
+//     }
+// });
